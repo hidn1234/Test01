48116e3105744eca441e0b72e1babd6285ab8287
WK <eng.wkim@icloud.com>
2020-06-09 11:16:19 +0900

    Updates

---
+++ precache-manifest.ed61589f8658e705f70ec76794cc8285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5ed75aa0207b3b8d2481136c521bde",
+    "url": "/Test01/index.html"
+  },
+  {
+    "revision": "118d8825c1415e477df0",
+    "url": "/Test01/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652ae790720b6d1cbf34",
+    "url": "/Test01/static/js/2.e31af3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test01/static/js/2.e31af3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118d8825c1415e477df0",
+    "url": "/Test01/static/js/main.a9917d6d.chunk.js"
+  },
+  {
+    "revision": "b87f485149436bc98ef7",
+    "url": "/Test01/static/js/runtime-main.1bbd1335.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test01/static/media/logo.5d5d9eef.svg"
+  }
+]);
